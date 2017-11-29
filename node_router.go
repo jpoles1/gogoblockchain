24d40fb1fd11bf4ad45fcf4d144aea63c823bfc5
Jordan Poles <jpoles1@gmail.com>
2017-11-29 16:24:06 -0500

    Implemented node functions.

--- node_router.go
+++ node_router.go
@@ -1,12 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 //Multinode routing
 func registerNode(w http.ResponseWriter, r *http.Request) {
-
+	params := mux.Vars(r)
+	fmt.Println(params)
+	addr, ok := r.URL.Query()["addr"]
+	if !ok || len(addr) < 1 {
+		http.Error(w, "Err 400: URL Param 'addr' is missing", 400)
+		return
+	}
+	servBlockchain.Nodes.add(Node{addr[0]})
+	w.Write([]byte("Added new blockchain node: " + addr[0]))
 }
 func resolveNode(w http.ResponseWriter, r *http.Request) {
-	servBlockchain.resolveConflicts()
-	//servBlockchain.validChain(servBlockchain.Chain)
+	msg := "Our chain is authoritative"
+	if servBlockchain.resolveConflicts() {
+		msg = "Our chain was replaced"
+	}
+	w.Write([]byte(msg))
 }
