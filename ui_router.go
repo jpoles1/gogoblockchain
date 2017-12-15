cb778b9ba173177292ec3a64378b5b6bc5b7b394
Jordan Poles <jpoles1@gmail.com>
2017-12-15 13:02:03 -0500

    Got poll selection and loading to work.

--- ui_router.go
+++ ui_router.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	template "github.com/kataras/go-template"
@@ -18,11 +19,21 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func pollPage(w http.ResponseWriter, r *http.Request) {
 	urlparams := mux.Vars(r)
 	pollID := urlparams["pollID"]
-	//if pollID != "" &&
-	voteOptions := []string{"Jordan Poles", "Maria D'lorio"}
-	err := template.ExecuteWriter(w, "vote.hbs", map[string]interface{}{"pollID": pollID, "voteopts": voteOptions}, renderOpts) // yes you can pass simple maps instead of structs
-	if err != nil {
-		w.Write([]byte(err.Error()))
+	pollStringID, _ := strconv.Atoi(pollID)
+	pollObj, ok := pollDict[pollStringID]
+	if ok {
+		err := template.ExecuteWriter(w, "vote.hbs", map[string]interface{}{"pollID": pollID, "voteopts": pollObj.Options}, renderOpts) // yes you can pass simple maps instead of structs
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+	} else {
+		err := template.ExecuteWriter(w, "redirect.hbs", map[string]interface{}{
+			"redir_msg": "Cannot find poll with this ID!",
+			"redir_url": "/",
+		}, renderOpts) // yes you can pass simple maps instead of structs
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
 	}
 }
 func newpollPage(w http.ResponseWriter, r *http.Request) {
